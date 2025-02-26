8ee196256d834e6716249e1abde718501038ecb4
ziyang <chaizy073@gmail.com>
2025-02-26 22:17:07 +0800

    feat: setter/getter for loop bw, analog gain, GPIO input/output and function

--- tas5827.h
+++ tas5827.h
@@ -55,11 +55,48 @@ const uint8_t REG_FAULT_CLEAR       = 0x78;
 class TAS5827
 {
 public:
+	/* Enums */
+	enum class LoopBW_t : uint8_t
+	{
+		LOOP_BW_80KHZ  = 0b00100000,
+		LOOP_BW_100kHZ = 0b00000000,
+		LOOP_BW_120kHZ = 0b01000000,
+		LOOP_BW_175kHZ = 0b01100000,
+	};
+
+	enum class GPIO_Mode_t : uint8_t
+	{
+		INPUT  = 0b0,
+		OUTPUT = 0b1,
+	};
+
+	enum class GPIO_Sel_t : uint8_t
+	{
+		OFF       = 0b0000,
+		WARNZ     = 0b1000,
+		FAULTZ    = 0b1011,
+		PVDD_DROP = 0b1100,
+		SDOUT     = 0b1101,
+		RAMP_CLK  = 0b1110,
+	};
 
 	TAS5827(void);
 	~TAS5827(void);
 
 	bool begin(uint8_t address, uint8_t DUMMY_I2C_HANDLE);
+
+	/* Setters */
+	bool setLoopBW(LoopBW_t loopBW);
+	bool setAnalogGain(uint8_t gain);
+	bool setGPIOMode(GPIO_Mode_t gpio_mode_0, GPIO_Mode_t gpio_mode_1, GPIO_Mode_t gpio_mode_2);
+	bool setGPIOSel(GPIO_Sel_t gpio_sel_0, GPIO_Sel_t gpio_sel_1, GPIO_Sel_t gpio_sel_2);
+
+	/* Getters */
+	bool getLoopBW(LoopBW_t* p_loopBW);
+	bool getAnalogGain(float* p_gain);
+	bool getPVDD(float* p_pvdd);
+	bool getGPIOMode(GPIO_Mode_t* p_gpio_mode_0, GPIO_Mode_t* p_gpio_mode_1, GPIO_Mode_t* p_gpio_mode_2);
+	bool getGPIOSel(GPIO_Sel_t* p_gpio_sel_0, GPIO_Sel_t* p_gpio_sel_1, GPIO_Sel_t* p_gpio_sel_2);
 
 private:
 	uint8_t address;
